4354d261989c480064707bd99d2eb049dcdcf7fb
mok <mok0230@protonmail.com>
2021-11-06 19:57:18 -0700

    implement high-level state to manage existing contracts

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,13 +2,16 @@ import './App.css';
 import NewContractForm from './NewContractForm';
 import ExistingContracts from './ExistingContracts';
 import { Container } from '@mui/material';
+import { useState } from 'react';
 
 function App() {
+  const [existingContracts, setExistingContracts] = useState([]);
+
   return (
     <Container>
       <h1>Simple Escrow Dapp</h1>
-      <ExistingContracts />
-      <NewContractForm />
+      <ExistingContracts existingContracts={existingContracts} />
+      <NewContractForm setExistingContracts={setExistingContracts} />
     </Container>
   );
 }
