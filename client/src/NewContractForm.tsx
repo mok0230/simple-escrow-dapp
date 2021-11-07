64944fbb4905bc86848b83dd54c18d065c662aae
mok <mok0230@protonmail.com>
2021-11-06 18:30:11 -0700

    persist new contract form fields in component state

--- client/src/NewContractForm.tsx
+++ client/src/NewContractForm.tsx
@@ -1,15 +1,34 @@
 import { Button, InputAdornment, Paper, TextField } from "@mui/material";
+import { useState } from "react";
 
 function NewContractForm() {
+  const [arbiterAddress, setArbiterAddress] = useState("");
+  const [beneficiaryAddress, setBeneficiaryAddress] = useState("");
+  const [depositAmount, setDepositAmount] = useState("");
+
+  // const deploy = async (arbiter, beneficiary, value) => {
+  //   await ethereum.request({ method: 'eth_requestAccounts' });
+  //   const signer = provider.getSigner();
+  //   const factory = new ethers.ContractFactory(Escrow.abi, Escrow.bytecode, signer);
+  //   return factory.deploy(arbiter, beneficiary, { value });
+  // }
+
+  const handleDeploy = () => {
+    console.log('handleDeploy');
+    console.log('arbiterAddress', arbiterAddress);
+    console.log('beneficiaryAddress', beneficiaryAddress);
+    console.log('depositAmount', depositAmount);
+  }
+
   return (
     <Paper elevation={3} sx={{p: 3}}>
       <h2> New Contract </h2>
-      <TextField label="Arbiter Address" variant="outlined" fullWidth sx={{ mb: 2 }} />
-      <TextField label="Beneficiary Address" variant="outlined" fullWidth sx={{ mb: 2 }} />
+      <TextField label="Arbiter Address" variant="outlined" fullWidth sx={{ mb: 2 }} value={arbiterAddress} onChange={e => setArbiterAddress(e.target.value)} />
+      <TextField label="Beneficiary Address" variant="outlined" fullWidth sx={{ mb: 2 }} value={beneficiaryAddress} onChange={e => setBeneficiaryAddress(e.target.value)} />
       <TextField label="Deposit Amount" variant="outlined" InputProps={{
             endAdornment: <InputAdornment position="end">Wei</InputAdornment>,
-          }} fullWidth sx={{ mb: 3 }} />
-      <Button variant="contained" size="large" fullWidth>Deploy</Button>
+          }} fullWidth sx={{ mb: 3 }} value={depositAmount} onChange={e => setDepositAmount(e.target.value)} />
+      <Button variant="contained" size="large" fullWidth onClick={handleDeploy}>Deploy</Button>
     </Paper>
   );
 }
