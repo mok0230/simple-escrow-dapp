ef8acffb44780898073eaf289b949babd41c6321
mok <mok0230@protonmail.com>
2021-11-06 19:59:15 -0700

    render no-frills JSON existing contract on contract deploy

--- client/src/ExistingContracts.tsx
+++ client/src/ExistingContracts.tsx
@@ -1,12 +1,12 @@
 import { Paper } from "@mui/material";
 
-function ExistingContracts() {
+function ExistingContracts({existingContracts}) {
   return (
     <Paper elevation={3} sx={{p: 3, mb:2}}>
       <h2> Existing Contracts </h2>
 
       <div id="container">
-          {/* where existing contracts will go */}
+          {JSON.stringify(existingContracts)}
       </div>
     </Paper>
   );
