19d4697aa02f4f6d770dd91392b6946f885890d4
mok <mok0230@protonmail.com>
2021-11-06 20:36:25 -0700

    implement data grid for existing contracts

--- client/src/ExistingContracts.tsx
+++ client/src/ExistingContracts.tsx
@@ -1,12 +1,42 @@
 import { Paper } from "@mui/material";
+import { DataGrid } from '@mui/x-data-grid';
 
+// TODO: define widths
+const columns = [
+  { 
+    field: 'address', 
+    headerName: 'Address' 
+  },
+  {
+    field: 'beneficiaryAddress',
+    headerName: 'Beneficiary',
+  },
+  {
+    field: 'arbiterAddress',
+    headerName: 'Arbiter',
+  },
+  {
+    field: 'value',
+    headerName: 'Value',
+    type: 'number',
+  }
+  // TODO: add approval
+];
+
+// TODO: validate height calculation below
 function ExistingContracts({existingContracts}) {
   return (
     <Paper elevation={3} sx={{p: 3, mb:2}}>
       <h2> Existing Contracts </h2>
 
-      <div id="container">
-          {JSON.stringify(existingContracts)}
+      {/* DataGrid "No rows" implementation is buggy and requires an explicit height */}
+      <div style={{ height: 150 + (50 * (Math.max(existingContracts.length - 1, 0))), width: '100%' }}>
+        <DataGrid
+        rows={existingContracts}
+        columns={columns}
+        pageSize={5}
+        // rowsPerPageOptions={[5]}
+      />
       </div>
     </Paper>
   );
