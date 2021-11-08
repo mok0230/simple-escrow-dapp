b50b54427920910d333829c75d6470a14043d896
mok <mok0230@protonmail.com>
2021-11-07 21:07:39 -0800

    reset new contract form on submission and provide light error handling

--- client/src/NewContractForm.tsx
+++ client/src/NewContractForm.tsx
@@ -1,4 +1,4 @@
-import { Button, InputAdornment, Paper, TextField } from "@mui/material";
+import { Alert, Button, InputAdornment, Paper, TextField } from "@mui/material";
 import { useState } from "react";
 import {ethers} from 'ethers';
 import Escrow from './artifacts/contracts/Escrow.sol/Escrow.json';
@@ -10,10 +10,24 @@ function NewContractForm({provider, setExistingContracts}) {
   const [arbiterAddress, setArbiterAddress] = useState("");
   const [beneficiaryAddress, setBeneficiaryAddress] = useState("");
   const [depositAmountEth, setDepositAmountEth] = useState("");
+  const [isFormValid, setIsFormValid] = useState(true);
+
+  const resetNewContractForm = () => {
+    setArbiterAddress("");
+    setBeneficiaryAddress("");
+    setDepositAmountEth("");
+  }
 
   const handleDeploy = async () => {
     console.log('handleDeploy');
-    console.log('setExistingContracts', setExistingContracts)
+    console.log('setExistingContracts', setExistingContracts);
+
+    if(!arbiterAddress || !beneficiaryAddress || !depositAmountEth) {
+      setIsFormValid(false);
+      return;
+    }
+
+    setIsFormValid(true);
     
     await ethereum.request({ method: 'eth_requestAccounts' });
 
@@ -35,6 +49,8 @@ function NewContractForm({provider, setExistingContracts}) {
         status: 'submitted'
       }
 
+      resetNewContractForm();
+
       setExistingContracts(existingContracts => [...existingContracts, contractData])
     } catch (e) {
       console.error('Error!', e);
@@ -44,12 +60,15 @@ function NewContractForm({provider, setExistingContracts}) {
   return (
     <Paper elevation={3} sx={{p: 3}}>
       <h2> New Contract </h2>
-      <TextField label="Arbiter Address" variant="outlined" fullWidth sx={{ mb: 2 }} value={arbiterAddress} onChange={e => setArbiterAddress(e.target.value)} />
-      <TextField label="Beneficiary Address" variant="outlined" fullWidth sx={{ mb: 2 }} value={beneficiaryAddress} onChange={e => setBeneficiaryAddress(e.target.value)} />
-      <TextField label="Deposit Amount" variant="outlined" InputProps={{
-            endAdornment: <InputAdornment position="end">ETH</InputAdornment>,
-          }} fullWidth sx={{ mb: 3 }} value={depositAmountEth} onChange={e => setDepositAmountEth(e.target.value)} />
-      <Button variant="contained" size="large" fullWidth onClick={handleDeploy}>Deploy</Button>
+      {isFormValid ? "" : <Alert severity="error" sx={{mb:2}}>Ensure all fields below contain input</Alert>}
+      <form>
+        <TextField required label="Arbiter Address" variant="outlined" fullWidth sx={{ mb: 2 }} value={arbiterAddress} onChange={e => setArbiterAddress(e.target.value)} />
+        <TextField required label="Beneficiary Address" variant="outlined" fullWidth sx={{ mb: 2 }} value={beneficiaryAddress} onChange={e => setBeneficiaryAddress(e.target.value)} />
+        <TextField required label="Deposit Amount" variant="outlined" InputProps={{
+              endAdornment: <InputAdornment position="end">ETH</InputAdornment>,
+            }} fullWidth sx={{ mb: 3 }} value={depositAmountEth} onChange={e => setDepositAmountEth(e.target.value)} />
+        <Button variant="contained" size="large" fullWidth onClick={handleDeploy}>Deploy</Button>
+      </form>
     </Paper>
   );
 }
